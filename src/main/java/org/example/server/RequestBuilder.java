5c0e3f9c4323d9cb39d962f532faa6a6096c75ce
Jana <100678972+ginger-darklord@users.noreply.github.com>
2022-09-28 12:22:31 +0200

    Initial commit

---
+++ src/main/java/org/example/server/RequestBuilder.java
@@ -0,0 +1,11 @@
+package org.example.server;
+
+public class RequestBuilder {
+
+    public static Request build(String Request){
+        // create a request from string Request
+        //request = Request;
+        //ab string builer nededs to be here
+        return new Request();
+    }
+}
